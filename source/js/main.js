17f534596728977f59aa4e523fedd816fb5314ca
Egorova Alexandra <alex.egorova.msk@gmail.com>
2023-03-12 23:41:56 +0600

    главный экран

--- source/js/main.js
+++ source/js/main.js
@@ -48,3 +48,37 @@ window.addEventListener('DOMContentLoaded', () => {
 // breakpointChecker();
 
 // используйте .closest(el)
+
+const itemsAccordion = document.querySelectorAll('.accordion__item');
+
+const removeState = () => {
+  itemsAccordion.forEach((item) => {
+    const toggle = item.querySelector('.accordion__toggle');
+
+    item.removeAttribute('data-state');
+    toggle.removeAttribute('data-nojs');
+  });
+};
+
+removeState();
+
+const initAccordion = () => {
+  itemsAccordion.forEach((item) => {
+    item.addEventListener('click', () => {
+      if (item.hasAttribute('data-state')) {
+        removeState();
+      } else {
+        removeState();
+        item.setAttribute('data-state', 'is-open');
+      }
+    });
+  });
+};
+
+initAccordion();
+
+
+window.addEventListener('resize', () => {
+  removeState();
+  initAccordion();
+});
